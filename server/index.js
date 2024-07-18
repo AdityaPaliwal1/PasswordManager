e241c4a4e0c7eb7b62d2a944c89860ca96c82e17
Aditya Paliwal <adityapaliwal243@gmail.com>
2024-07-18 20:04:09 +0530

    Connecting Mysql Db and inserted Values in it via frontend

--- server/index.js
+++ server/index.js
@@ -1,13 +1,40 @@
-const express = require('express');
+const express = require("express");
+const mysql = require("mysql");
 const app = express();
-const colors = require('colors');
-const cors = require('cors');
+const colors = require("colors");
+const cors = require("cors");
 const Port = 5000;
 
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "#aditya_2004",
+  database: "passwordmanager",
+});
 app.listen(Port, () => {
-    console.log(`Server is running on port ${Port}`.yellow.bold);
+  console.log(`Server is running on port ${Port}`.yellow.bold);
 });
 
-app.get('/', (req, res) => {
-    res.send('Server is Live😄');
+app.get("/", (req, res) => {
+  res.send("Server is Live😄");
+});
+
+app.post("/addpasswords", (req, res) => {
+  const { password, title } = req.body;
+
+  db.query(
+    "INSERT INTO passwords (passwords , title) VALUES (?,?)",
+    [password, title],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        console.log("Failed to insert values");
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
 });
